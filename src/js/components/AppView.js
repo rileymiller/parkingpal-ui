524ce808953e7f9fe8aca75527b02380950d17c4
Riley Miller <rileymiller@mymail.mines.edu>
2018-11-15 10:01:51 -0700

    parking lot mocks

--- src/js/components/AppView.js
+++ src/js/components/AppView.js
@@ -18,30 +18,59 @@ class AppView extends Component {
       title: event.target.value
     });
   }
-  Lots = new Array()
-  Lots = [
-    {name: "K", num: 67},
-    {name: "D", num: 43},
-    {name: "M", num: 42},
-    {name: "Q", num: 10},
-    {name: "CT", num:22},
-    {name: "I", num: 33}
-  ]
+  // Lots = new Array()
+  // Lots = [
+  //   {name: "K", num: 67},
+  //   {name: "D", num: 43},
+  //   {name: "M", num: 42},
+  //   {name: "Q", num: 10},
+  //   {name: "CT", num:22},
+  //   {name: "I", num: 33}
+  // ]
 
-  LotViews = Lots.map((lot) => {
-    <LotView
-        key={lot.name}
-        name={lot.name}
-        num={lot.num}
-      />
-  })
+  // LotViews = Lots.map((lot) => {
+  //   <LotView
+  //       key={lot.name}
+  //       name={lot.name}
+  //       num={lot.num}
+  //     />
+  // })
 
   render() {
 
     return (
       <div>
         <h2>{this.state.title}</h2>
-          {LotViews}
+        <LotView
+          key={"K"}
+          name={"K"}
+          num={67}
+        />
+        <LotView
+          key={"D"}
+          name={"D"}
+          num={43}
+        />
+        <LotView
+          key={"M"}
+          name={"M"}
+          num={42}
+        />
+        <LotView
+          key={"Q"}
+          name={"Q"}
+          num={10}
+        />
+        <LotView
+          key={"CT"}
+          name={"CT"}
+          num={22}
+        />
+        <LotView
+          key={"I"}
+          name={"I"}
+          num={33}
+        />
       </div>
     );
   }
